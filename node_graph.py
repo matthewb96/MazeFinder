16bbea9672455ea433a9299df52091c3b75cad7f
Matthew <matthewbuckley051@gmail.com>
2019-05-06 21:19:22 +0100

    Creating empty files
    Created new empty files for the pathfinding algorithms and the classes for the node graph datastructure

---
+++ node_graph.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module containing the classes for making nodes and graphs.
+"""
+##### IMPORTS #####
+
+
+##### CLASSES #####
+
+
+##### FUNCTIONS #####
+
+
+##### MAIN #####
+
---
+++ pathfinding.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module for performing a pathfinding algorithm on a node_graph.
+"""
+##### IMPORTS #####
+
+
+##### CLASSES #####
+
+
+##### FUNCTIONS #####
+
+
+##### MAIN #####
+
+
